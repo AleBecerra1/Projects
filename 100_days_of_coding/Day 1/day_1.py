4d878bd3e984f3027bc26d3c84845668a9b4f48c
Alejandro Becerra <alejandronicolasbecerra@gmail.com>
2025-02-10 10:44:01 -0300

    Second project solved

--- 100_days_of_coding/Day 1/day_1.py
+++ 100_days_of_coding/Day 1/day_1.py
@@ -1,27 +1,4 @@
-#Exercise 1
-print("Hello Wolrd")
-
-#Exercise 2
-print("Hello World!\nHello World!\nHello World!")
-print("Hello" + " " + "Angela")
-
-#Exercise 3
-print("Hello " + input("What is your name?") + "!")
-
-#Excercise 4
-input("What is your name?")
-print(len(input("What is your name?")))
-
-name = input("What is your name?")
-characters = len(name)
-print(characters)
-
-#Excercise 5
-name = "Angela"
-length = len(name)
-print(length)
-
-#Excercise 6
+#Project 1
 print("Hi welcome to the Band Name Generator!")
 city_name = input("What's the name of the city you grew up in?")
 pet_name = input("What's your pet's name?")
